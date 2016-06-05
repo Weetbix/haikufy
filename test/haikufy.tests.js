7cc72cff8f663ba3e266db0bf0b6bd394f10ed64
John Hannagan <john@email.com>
2016-06-05 17:08:49 +0200

    Initial haiku algorithm

--- test/haikufy.tests.js
+++ test/haikufy.tests.js
@@ -1,7 +1,22 @@
-var should = require('chai').should();
+var expect = require('chai').expect;
+var Haikufy = require('../lib/haikufy');
+
+var haikufy = new Haikufy();
 
 describe('Haikus', function(){
-   it('should pass', function(){
-       
-   });
+    
+    describe('with single sentences', function(){
+        
+        it('should identify the haiku with no punctuation', function(){
+             var input = 'In a pouch I   grow on a southern continent strange creatures I know';
+             var expected = [
+                 'In a pouch I grow',
+                 'on a southern continent',
+                 'strange creatures I know'
+             ];
+             
+             expect(haikufy.find(input)).to.exist;
+             expect(haikufy.find(input)).to.deep.equal.expected;
+        });
+    })
 });
