bc8b51a4dfd7cd535eead98f165fee6dd4842998
John Hannagan <weetbix_@hotmail.com>
2016-06-06 20:15:30 +0200

    Add failing haiku example

--- test/haikufy.tests.js
+++ test/haikufy.tests.js
@@ -3,11 +3,11 @@ var Haikufy = require('../lib/haikufy');
 
 var haikufy = new Haikufy();
 
-describe('Haikus', function(){
+describe('Haikus', () => {
     
-    describe('with single sentences', function(){
+    describe('with single sentences', () => {
         
-        it('should identify the haiku with no punctuation', function(){
+        it('should identify the haiku with no punctuation', () =>{
              var input = 'In a pouch I grow on a southern continent strange creatures I know';
              var expected = [
                  'In a pouch I grow',
@@ -19,7 +19,7 @@ describe('Haikus', function(){
              expect(haikufy.find(input)).to.deep.equal(expected);
         });
         
-        it('should identify the haiku with punctuation', function(){
+        it('should identify the haiku with punctuation', () => {
              var input = 'In a pouch I grow, on a southern continent! strange creatures I know.';
              var expected = [
                  'In a pouch I grow,',
@@ -31,7 +31,7 @@ describe('Haikus', function(){
              expect(haikufy.find(input)).to.deep.equal(expected);
         });
         
-        it('should identify clear whitespace at the ends of stanzas', function(){
+        it('should identify clear whitespace at the ends of stanzas', () => {
              var input = 'In a pouch I grow     on a southern continent    strange creatures I know';
              var expected = [
                  'In a pouch I grow',
@@ -43,8 +43,8 @@ describe('Haikus', function(){
              expect(haikufy.find(input)).to.deep.equal(expected);
         });
         
-        describe('where syllable breaks are not ends of words', function(){
-            it('should not be identified as a haiku in first split', function(){
+        describe('where syllable breaks are not ends of words', () => {
+            it('should not be identified as a haiku in first split', () => {
                 // Stanza now goes 6 - 6 - 5
                 //                    5/6   <- 5/6 split is on a word, this shouldnt be a haiku
                 var input = 'a a a a mammoth   a a a a a a   a a a a a';
@@ -52,7 +52,7 @@ describe('Haikus', function(){
                 expect(haikufy.find(input)).to.be.null;
             });
             
-            it('should not be identified as a haiku in second split', function(){
+            it('should not be identified as a haiku in second split', () => {
                 // Stanza now goes 5 - 8 - 4
                 //                                   12/13 <- split on word
                 var input = 'a a a a a  b b b b b b mammoth  a a a a';
@@ -60,7 +60,7 @@ describe('Haikus', function(){
                 expect(haikufy.find(input)).to.be.null;
             });
             
-            it('should not be identified as a haiku when both splts are words', function(){
+            it('should not be identified as a haiku when both splts are words', () => {
                 // Stanza now goes 5 - 8 - 4
                 //                                   12/13 <- split on word
                 var input = 'a a a a mammoth   a a a a a mammoth   a a a a';
@@ -68,6 +68,20 @@ describe('Haikus', function(){
                 expect(haikufy.find(input)).to.be.null;
             });
         })
-
     })
+    
+    describe('real examples', () => {
+        describe('that should fail', () => {
+            const test_cases = [
+                `also discovered there's a huge custom dlc forum with shitloads of songs`
+            ];
+            
+            test_cases.forEach( (test, index) => {
+                index++;
+                it(`test case ${index} should not be identified as a haiku`, () => {
+                    expect(haikufy.find(test)).to.be.null;s
+                });
+            })
+        });
+    });
 });
